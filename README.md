0aa21880f172ffb551224c29da8d646cfc5ebbca
Isabelle <meguminperson@gmail.com>
2024-03-01 15:05:29 +0000

    feat: The code :sparkles:

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# per-user-colour-roles
- Simple custom colour role per user - only supports max 250 users
+# Per user colour roles
+Simple custom colour role per user - only supports max 250 users (discord guild role limit)
+
+# Setup
+1. Install Python3.12 (my beloved)
+2. Create a Python venv `python -m venv env`
+3. Activate venv `source env/bin/activate` (linux) or `env/Scripts/activate` (windows)
+4. Install requirements `python -m pip install requirements.txt`
+5. Rename `example.env` to `.env`
+6. Replace `DISCORD_BOT_TOKEN = "TOKEN_HERE"` with your token as `TOKEN_HERE`
+7. Run the bot with `python colours.py`.
+8. To run it constantly look into `nohup` or `screen` (linux) :3
+
+PRs are welcome.
---
+++ colours.py
@@ -0,0 +1,128 @@
+from os import getenv
+from typing import Final, Literal, TypeAlias
+
+from disnake import GuildCommandInteraction, Member, Role, User
+from disnake.ext.commands import InteractionBot, Param, guild_only
+from dotenv import load_dotenv
+
+
+Hex: TypeAlias = "int"
+VALID_HEX_CHARS: Final[Literal["0123456789abcdef"]] = "0123456789abcdef"
+LEN_OF_HEX_STR: Final[int] = 6
+
+
+bot = InteractionBot()
+member_roles: dict[int, Role] = {}
+
+
+def _get_user_role_name(member: Member | User) -> str:
+    """Generates the users role name"""
+    # Change this to whatever you want, just make sure that it's
+    #   unique per user, consistent, and below 250 chars (role name max len).
+    return str(member.id)
+
+
+async def _fetch_users_role(member: Member | User) -> Role | None:
+    """Fetches the users colour role"""
+    if not isinstance(member, Member):
+        return None
+
+    user_role_name = _get_user_role_name(member)
+    role = member_roles.get(member.id, None)
+    if role is not None:
+        return role
+
+    for role in member.guild.roles:
+        if role.name == user_role_name:
+            member_roles[member.id] = role
+            return role
+    return None
+
+
+def _convert_str_hex_to_int_hex(str_hex: str) -> Hex | None:
+    """Converts a str representation of a hex int to an int representation of a hex int"""
+
+    str_hex = str_hex.lower()
+    colour = str_hex if (str_hex[0] != "#") else str_hex[1::]
+    colour = colour if (str_hex[0:2] != "0x") else str_hex[2::]
+    if not (all(c in VALID_HEX_CHARS for c in colour) or len(colour) != LEN_OF_HEX_STR):
+        return None
+    else:
+        return int(colour, 16)
+
+
+@guild_only()
+@bot.slash_command(
+    name="colour-role",
+    description="Edit / create your custom colour role.",
+)
+async def colour_role(_: GuildCommandInteraction[InteractionBot]) -> None:
+    pass
+
+@colour_role.sub_command(
+    name="set-colour",
+    description="Set the colour of your colour role.",
+)
+async def colour_role_set_colour(
+    inter: GuildCommandInteraction[InteractionBot],
+    colour: str = Param(
+        name="colour",
+        description="The hex code of the colour."
+    ),
+) -> None:
+    hex_int = _convert_str_hex_to_int_hex(colour)
+    if hex_int is None:
+        await inter.response.send_message(
+            content="That colour was not a valid hex code D:",
+            ephemeral=True,
+        )
+        return
+
+    await inter.response.defer()
+
+    role = await _fetch_users_role(inter.user)
+    if role is None:
+        user_role_name = _get_user_role_name(inter.user)
+        role = await inter.guild.create_role(
+            name=user_role_name,
+            colour=hex_int,
+            reason="User requested colour role."
+        )
+    else:
+        if role.colour.value != hex_int:
+            await role.edit(
+                colour=hex_int,
+                reason="User requested new colour.",
+            )
+    await inter.edit_original_response(
+        content="Your colour role has been updated :D",
+    )
+
+@colour_role.sub_command(
+    name="delete-role",
+    description="Delete your colour role.",
+)
+async def colour_role_delete_colour(
+    inter: GuildCommandInteraction[InteractionBot],
+) -> None:
+    await inter.response.defer()
+    role = await _fetch_users_role(inter.user)
+    if role is None:
+        await inter.edit_original_response(
+            content="You don't have a colour role!",
+        )
+        return
+
+    await role.delete(reason="Requested by role owner.")
+    await inter.edit_original_response(
+        content="Your colour role has been deleted!",
+    )
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    discord_bot_token: str | None = getenv("DISCORD_BOT_TOKEN")
+    if discord_bot_token is None:
+        raise EnvironmentError("`DISCORD_BOT_TOKEN` was not provided.")
+
+    bot.run(discord_bot_token)
